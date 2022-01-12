e365aaa0a4c526f119265638b930ed16523dae8b
Sonia <sonia.livet@orange.fr>
2022-01-12 22:07:52 +0100

    quizz terminé

--- quizz-ados.js
+++ quizz-ados.js
@@ -1,2 +1,116 @@
-const form=document.querySelectorAll('.form-quizz .question-block');
-console.log(form);
+const form = document.querySelector('.form-quizz');
+let tableauResultats = [];
+const reponses = ['a','a','a','a'];
+const emojis = ['✔️','✨','😭','👎'];
+const titreResultat = document.querySelector('.résultats h3');
+const noteResultat = document.querySelector('.note');
+const aideResultat = document.querySelector('.aide');
+const toutesLesQuestions = document.querySelectorAll('.question-block');
+let verifTableau = [];
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    // console.log(document.querySelector('input[name="q1"]:checked').value);
+
+    for(i = 1; i < 5; i++) {
+        tableauResultats.push(document.querySelector(`input[name="q${i}"]:checked`).value)
+    }
+    // console.log(tableauResultats);
+    verifFunc(tableauResultats);
+    tableauResultats = [];
+})
+
+function verifFunc(tabResultats) {
+
+    for(let a = 0; a < 4; a++){
+
+        if(tabResultats[a] === reponses[a]) {
+            verifTableau.push(true);
+        } else {
+            verifTableau.push(false);
+        }
+
+    }
+
+    // console.log(verifTableau);
+    afficherResultats(verifTableau);
+    couleursFonction(verifTableau);
+    verifTableau = [];
+}
+
+function afficherResultats(tabCheck) {
+
+    const nbDeFautes = tabCheck.filter(el => el !== true).length;
+    // console.log(nbDeFautes);
+
+    switch(nbDeFautes) {
+
+        case 0:
+            titreResultat.innerText = `✔️ Bravo, c'est un sans faute ! ✔️`
+            aideResultat.innerText = ''
+            noteResultat.innerText = '4/4'
+            break;
+        case 1:
+            titreResultat.innerText = `✨ Vous y êtes presque ! ✨`
+            aideResultat.innerText = 'Retentez une autre réponse dans la case rouge, puis re-validez !'
+            noteResultat.innerText = '3/4'
+            break;
+        case 2:
+            titreResultat.innerText = `👀 Il reste quelques erreurs. 😭`
+            aideResultat.innerText = 'Retentez une autre réponse dans les cases rouges, puis re-validez !'
+            noteResultat.innerText = '2/4'
+            break;
+        case 3:
+            titreResultat.innerText = `😭 Peux mieux faire ! 😭`
+            aideResultat.innerText = 'Retentez une autre réponse dans les cases rouges, puis re-validez !'
+            noteResultat.innerText = '1/4'
+            break;
+        case 4:
+            titreResultat.innerText = `👎 Peux mieux faire ! 👎`
+            aideResultat.innerText = 'Retentez une autre réponse dans les cases rouges, puis re-validez !'
+            noteResultat.innerText = '0/4'
+        break;
+
+        default:
+            'Woops, cas innatendu.';
+
+    }
+
+}
+
+
+function couleursFonction(tabValBool) {
+
+    for(let j = 0; j < tabValBool.length; j++){
+
+        if(tabValBool[j] === true){
+            toutesLesQuestions[j].style.background = 'lightgreen';
+        } else {
+            toutesLesQuestions[j].style.background = '#ffb8b8';
+            toutesLesQuestions[j].classList.add('echec');
+
+            setTimeout(() => {
+                toutesLesQuestions[j].classList.remove('echec');
+            }, 500)
+        }
+        
+    }
+
+}
+
+toutesLesQuestions.forEach(item => {
+    item.addEventListener('click', () => {
+        item.style.background = "white";
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
